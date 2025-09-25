0f163a02325b117d689a0706ef0476455445d7ee
boholder <bottleholder@anche.no>
2025-09-25 10:27:48 +0800

    temp

--- src/main.cpp
+++ src/main.cpp
@@ -1,20 +1,21 @@
 #include <Arduino.h>
+#include <bb_spi_lcd.h>
 
-#include "rymcu_esp32_s3_devkitc1.h"
+BB_SPI_LCD lcd;
 
 
 void setup()
 {
-    for (const int i : GPIO_PIN_LIST)
-    {
-        pinMode(i, OUTPUT);
-    }
+  lcd.begin(DISPLAY_WS_LCD_169);
+  lcd.setRotation(90);
+  // The default orientation is portrait. To set landscape mode use setRotation(270)
+  lcd.fillScreen(TFT_BLACK); // the default is to not keep a local copy of the framebuffer since it's large
+  lcd.setTextColor(TFT_GREEN); // all functions will send data to the display and be visible immediately
+  lcd.setFont(FONT_12x16);
+  lcd.println("Generic SPI display");
+  lcd.println("Using bb_spi_lcd");
 }
 
 void loop()
 {
-    for (const int i : GPIO_PIN_LIST)
-    {
-        digitalWrite(i, HIGH);;
-    }
 }
