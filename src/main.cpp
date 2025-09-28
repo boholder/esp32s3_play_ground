f872208ed6534368f497ead8be7b003af74dbb09
boholder <bottleholder@anche.no>
2025-09-28 18:15:22 +0800

    feat: lvgl demo runs on native

--- src/main.cpp
+++ src/main.cpp
@@ -1,21 +1,32 @@
+#include "lvgl.h"
+#include "app_hal.h"
+
+#include "demos/lv_demos.h"
+
+#ifdef ARDUINO
 #include <Arduino.h>
-#include <bb_spi_lcd.h>
 
-BB_SPI_LCD lcd;
-
-
-void setup()
-{
-  lcd.begin(DISPLAY_WS_LCD_169);
-  lcd.setRotation(90);
-  // The default orientation is portrait. To set landscape mode use setRotation(270)
-  lcd.fillScreen(TFT_BLACK); // the default is to not keep a local copy of the framebuffer since it's large
-  lcd.setTextColor(TFT_GREEN); // all functions will send data to the display and be visible immediately
-  lcd.setFont(FONT_12x16);
-  lcd.println("Generic SPI display");
-  lcd.println("Using bb_spi_lcd");
+void setup() {
+  lv_init();
+  hal_setup();
+  lv_demo_widgets();
 }
 
-void loop()
-{
+void loop() {
+  hal_loop();  /* Do not use while loop in this function */
 }
+
+#else
+
+int main(void)
+{
+	lv_init();
+
+	hal_setup();
+
+  lv_demo_widgets();
+
+	hal_loop();
+}
+
+#endif /*ARDUINO*/
